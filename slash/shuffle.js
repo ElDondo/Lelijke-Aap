4a147ecaaaed62539842b4bae5bacd86a0a40cfa
Kevin <krefin1@hotmail.com>
2023-03-17 20:59:54 +0100

    updated shuffle command

--- slash/shuffle.js
+++ slash/shuffle.js
@@ -1,15 +1,17 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { useQueue } = require("discord-player")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("shuffle")
         .setDescription("Shuffles the queue"),
     run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId)
+        const queue = useQueue(interaction.guild.id)
+        if (queue.isEmpty()) {
+            return await interaction.editReply("There are no songs in the queue.")
+        }
 
-        if (!queue) return await interaction.editReply("There are no songs in the queue")
-
-        queue.shuffle()
-        await interaction.editReply(`The queue of ${queue.tracks.length} songs have been shuffled!`)
+        queue.tracks.shuffle()
+        await interaction.editReply(`The queue of ${queue.getSize()} songs have been shuffled!`)
     }
 }
