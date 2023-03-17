04654ac819daa921148f3a55c43a003ad2dc1791
Kevin <krefin1@hotmail.com>
2023-03-17 20:31:31 +0100

    update pause command

--- slash/pause.js
+++ slash/pause.js
@@ -1,15 +1,17 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { useQueue } = require("discord-player")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pause")
         .setDescription("Pauses the music"),
     run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId)
+        const queue = useQueue(interaction.guild.id)
+        if (queue.isEmpty() && !queue.isPlaying()) {
+            return await interaction.editReply("There are no songs in the queue.")
+        }
 
-        if (!queue) return await interaction.editReply("There are no songs in the queue")
-
-        queue.setPause(true)
+        queue.node.setPaused(!queue.node.isPaused())
         await interaction.editReply("Music has been paused! Use `/resume` to resume the music")
     }
 }
