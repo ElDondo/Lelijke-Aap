5537486eaa755b691411c4e0660e14db7ae6dbf5
Kevin <krefin1@hotmail.com>
2023-03-17 20:32:11 +0100

    update resume command

--- slash/resume.js
+++ slash/resume.js
@@ -1,15 +1,17 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { useQueue } = require("discord-player")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("resume")
         .setDescription("Resumes the music"),
     run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId)
+        const queue = useQueue(interaction.guild.id)
+        if (queue.isEmpty() && !queue.isPlaying()) {
+            return await interaction.editReply("There are no songs in the queue.")
+        }
 
-        if (!queue) return await interaction.editReply("There are no songs in the queue")
-
-        queue.setPause(false)
+        queue.node.setPaused(!queue.node.isPaused())
         await interaction.editReply("Music has been paused! Use `/pause` to resume the music")
     }
 }
