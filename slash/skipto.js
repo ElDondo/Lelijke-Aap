33518688b34cf29b97ebed32bb7655ca09f6f4c5
Kevin <krefin1@hotmail.com>
2023-03-17 21:00:05 +0100

    updated skipto command

--- slash/skipto.js
+++ slash/skipto.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
+const { useQueue } = require("discord-player")
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,17 +7,20 @@ module.exports = {
         .setDescription("Skips to a certain track #")
         .addNumberOption((option) => option.setName("tracknumber").setDescription("The track to skip to").setMinValue(1).setRequired(true)),
     run: async ({ client, interaction }) => {
-        const queue = client.player.getQueue(interaction.guildId)
+        const queue = useQueue(interaction.guild.id)
 
-        if (!queue) return await interaction.editReply("There are no songs in the queue")
+        if (queue.isEmpty() && !queue.isPlaying()) {
+            return await interaction.editReply("There are no songs in the queue.")
+        }
 
         const trackNum = interaction.options.getNumber("tracknumber")
-        if (trackNum > queue.tracks.length){
+        if (trackNum > queue.getSize()){
             return await interaction.editReply("Invalid track number")
         }
-        
-        await interaction.editReply(`Skipped ahead to track number ${trackNum} - ${queue.tracks[trackNum-1].title}`)
-        queue.skipTo(trackNum - 1)
+        const tracks = queue.tracks.toArray()
+        console.log(tracks[trackNum-1].title)
+        await interaction.editReply(`Skipped ahead to track number ${trackNum} - ${tracks[trackNum-1].title}`)
+        queue.node.skipTo(trackNum - 1)
     }
 }
     
